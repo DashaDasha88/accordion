3b22bfe7ad0a2847840f3eb7cdb031a55780e725
Dasha <dasha.kotova.mail@gmail.com>
2021-08-16 20:33:35 -0400

    add styles

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Data } from "./Data";
+import styled from "styled-components";
+import { IconContext } from "react-icons";
+import { FiPlus, FiMinus } from "react-icons/fi";
+
+const AccordionSection = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  background: #fff;
+`;
+
+const Container = styled.div `
+  position: absolute;
+  top: 30%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+
+const Wrap = styled.div `
+  background: #272727;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+
+  h1 {
+    padding: 2rem;
+    font-size: 2rem;
+  }
+`;
 
 const Accordion = () => {
+
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = index => {
+
+    if(clicked === index) {
+      //if clicked question is already active, then close it
+
+      return setClicked(null)
+    }
+
+  }
+
   return (
-    <div>
-      accordion
-    </div>
-  )
-}
+    <IconContext.Provider value={{ color: '#00FF89', size: '25px'}}>
+
+      <AccordionSection>
+        <Container>
+          {Data.map((item, index) => {
+
+            return (
+              <>
+              <Wrap>
+                <h1>{item.question}</h1>
+              </Wrap>
+                <p>{item.answer}</p>
+              </>
+            );
+
+          })};
+
+        </Container>
+      </AccordionSection>
+
+    </IconContext.Provider>
+  );
+};
 
 export default Accordion;
